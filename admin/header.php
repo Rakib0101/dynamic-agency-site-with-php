96e07990b404fb1f8a47781259f35dfdaf328356
Rakib0101 <rakib.01jan@gmail.com>
2022-03-26 12:18:04 +0600

    design dashboard header

---
+++ admin/header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <!-- Vendor CSS Files -->
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- fontawesome cdn -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" integrity="sha384-/frq1SRXYH/bSyou/HUp/hib7RVN1TawQYja658FEOodR/FQBKVqT9Ol+Oz3Olq5" crossorigin="anonymous">
+
+    <link href="../assets/css/main.css" rel="stylesheet">
+</head>
+<body>
+    <div class="top-bar">
+        <div class="row border-1 border-bottom container-fluid">
+            <div class="col-md-2 border-1 border-end py-2">
+                <a class="d-block text-left text-decoration-none" href=""><h4>Site Logo</h4></a>
+            </div>
+            <div class="col-md-10 py-2">
+                <div class="row">
+                    <div class="col-8">
+                        <input class="w-100" type="text" placeholder="search here ...">
+                    </div>
+                    <div class="col-4">
+                        <div class="row">
+                            <div class="col-6 d-flex justify-content-around">
+                                <div class="px-2"><i class="fa-regular fa-message"></i></div>
+                                <div class="px-2"><i class="fa-regular fa-bell"></i></div>
+                            </div>
+                            <div class="col-6">
+                                <a href="">My Account</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row container-fluid main-section">
+        <div class="col-md-2 sidebar">
+            <ul>
+                <li>
+                    <a href="#"><i class="fa-solid fa-house-user"></i> <span>Dashboard</span></a>
+                </li>
+                <li>
+                    <a href="hero-section.html">Hero Section</a>
+                </li>
+                <li>
+                    <a href="#">About Us Section</a>
+                </li>
+                <li>
+                    <a href="#">Service Section</a>
+                </li>
+                <li>
+                    <a href="#">Portfolio Section</a>
+                </li>
+                <li>
+                    <a href="#">Package Section</a>
+                </li>
+                <li>
+                    <a href="#">Faq Section</a>
+                </li>
+                <li>
+                    <a href="#">Team Section</a>
+                </li>
+                <li>
+                    <a href="#">Contact Section</a>
+                </li>
+            </ul>
+        </div>
